667fa0323a614d3da53945ddc3c6e33215dda51d
AntonMartic <anton.martic@gmail.com>
2025-06-30 22:14:30 +0200

    handle spliting of h1 string and async scrape

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -11,29 +12,39 @@ func main() {
 	// Create a new collector
 	c := colly.NewCollector(
 		colly.AllowedDomains("studieinfo.liu.se"),
+		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"),
 	)
-
-	// Testing the collector with h2 on first page
-	c.OnHTML("h2.overview-label", func(e *colly.HTMLElement) {
-		fmt.Println("h2-tags: ", e.Text)
-	})
 
 	// Print visited URLs
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting:", r.URL.String())
 	})
 
-	// Test to print content on "Programplan"
-	/* c.OnHTML("div.tab-pane#curriculum", func(e *colly.HTMLElement) {
-		fmt.Println("\nCurriculum Content Found!")
-		fmt.Println("----------------------")
-		fmt.Println(e.Text)
-	}) */
+	var currentProgram, currentCredits string
 
-	// Print semesters on "Programplan" page
+	// Capture h1 separately (async)
+	c.OnHTML("h1", func(e *colly.HTMLElement) {
+		parts := strings.Split(strings.TrimSpace(e.Text), ",")
+		if len(parts) >= 2 {
+			currentProgram = strings.TrimSpace(parts[0])
+			currentCredits = strings.TrimSpace(parts[1])
+		}
+	})
+
+	// Handle "Programplan" page
 	c.OnHTML("div.tab-pane#curriculum", func(e *colly.HTMLElement) {
-		fmt.Println("\nFound Syllabus Section:")
+		fmt.Println("\n Curriculum section:")
 
+		// Print URL
+		currentURL := e.Request.URL.String()
+		fmt.Println("\nCurrent Page URL:", currentURL)
+
+		// Print program and credits
+		if currentProgram != "" {
+			fmt.Printf("Program: %s\nCredits: %s\n", currentProgram, currentCredits)
+		}
+
+		// Loop and print all semsesters
 		e.ForEach("h3", func(_ int, h3 *colly.HTMLElement) {
 			fmt.Printf("H3: %s\n", h3.Text)
 		})
