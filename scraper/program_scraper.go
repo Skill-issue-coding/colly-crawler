9cadb7c5110e43507c810b6c9aed60d4845703bd
AntonMartic <anton.martic@gmail.com>
2025-07-27 22:51:39 +0200

    Add Program scraper function

---
+++ scraper/program_scraper.go
@@ -0,0 +1,20 @@
+package scraper
+
+import (
+	/* "encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"sync"
+	"time"
+	*/
+	"github.com/Skill-issue-coding/colly-crawler/models"
+	/* "github.com/gocolly/colly/v2" */)
+
+func ScrapeProgram(url string) (models.Program, error) {
+	// test
+	return models.Program{}, nil
+}
